712ab165c9e9749d233209957d60a343ca9f4248
tavohm <gustavo.hernandez.cbtis94@gmail.com>
2020-03-30 15:06:48 -0600

    Código base. Ciclos para tabla de multiplicar.

---
+++ 01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    for(a=1; a<=2; a=a+1){
+        for(b=1; b<=10; b=b+1){
+            printf("%d x %d = %d\n",a,b,a*b);
+        }
+        printf("\n");
+    }
+    return 0;
+}
