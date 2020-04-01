ce75cddce65b479d0ca3481eb561fd3cae6bedc7
tavohm <gustavo.hernandez.cbtis94@gmail.com>
2020-04-01 10:34:12 -0600

    Ciclo FOR

---
+++ 02.c
@@ -0,0 +1,10 @@
+ #include <stdio.h>
+
+ int main(){
+     int i;
+     for( i=10; i>=0; i=i-1) //incremento: i=i+1 i=i+5 decremento: i=i-1 i=i-5
+     {
+         printf("%d ",i);
+     }
+     return 0;
+ }
