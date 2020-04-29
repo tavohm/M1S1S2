4ba1573118166e8661612b2893c91a2909e97e0b
tavohm <gustavo.hernandez.cbtis94@gmail.com>
2020-04-29 11:10:47 -0500

    Se agregó el ejemplo de funciones y librería

---
+++ 10.c
@@ -0,0 +1,24 @@
+//Ejercicio demostrativo de funciones en C, función para sumar dos números enteros
+
+void suma(int, int); //protipo de la función, no regresa valores
+
+#include <stdio.h>
+#include "10.h"
+
+int main(){
+
+    int a,b;
+    
+    printf("Dime el primer valor: ");
+    scanf("%d",&a);
+    printf("Dime el segundo valor: ");
+    scanf("%d",&b);
+
+    suma(a,b);
+    printf("\nUsando la funcion resta de la libreria: %d",resta(a,b));
+    return 0;
+}
+
+void suma(int a, int b){
+    printf("Suma: %d + %d = %d",a,b,a+b);
+}
